46d9411e9998708f12cf3f32c56e98badd9e6535
Cristoffer Fairweather <developer@cfairweather.com>
2016-04-17 16:40:13 -0700

    Adding and testing support for multiple proxies and changing TLS_SETTINGs.

--- ReadMe.md
+++ ReadMe.md
@@ -9,8 +9,19 @@ Docker Hub image: [docker-nginx-letsencrypt-proxy](https://hub.docker.com/r/anni
 The following docker environment variables are required for proper usage:
 - `LE_DOMAIN`, a comma separated list of domains current configured to point at your server
 - `LE_EMAIL`, the email address for use with Let's Encrypt (simply registers your public key for retrieval).
-- `PROXY_DEST`, the destination for the proxied service; along the lines of `http://mydestination.com` or `http://localhost:8000`
+- `PROXY_DEST`, a comma separated list of destinations for the proxied services; along the lines of `http://mydestination.com` or `http://localhost:8000`. There should be as many destinations as `LE_DOMAIN`s; however, for each without a corresponding destination, the first destination will be used for the remaining `LE_DOMAIN`s.
 - `SLACK_NOTIFICATIONS_INFRA_URL` (optional), the slack webhook integration URL to receive slack notifications upon certificate update or `letsencrypt-auto` error.
+- `TLS_SETTING` (optional), one of `MODERN`, `INTERMEDIATE`, OR `OLD`. All other values will be igored. `MODERN` is default to allow for the best security setting.
+  - See [https://wiki.mozilla.org/Security/Server_Side_TLS](https://wiki.mozilla.org/Security/Server_Side_TLS) for more details
+  - See [docker-entrypoint.sh](https://github.com/Annixa/docker-nginx-letsencrypt-proxy/blob/master/docker-entrypoint.sh) for the suites used
+  - Updated April 17, 2016
+  - This setting will correspond to the following browser compatibilities:
+  
+| Configuration | Oldest compatible client | 
+| ------------- |:------------------------|
+| `MODERN` | Firefox 27, Chrome 30, IE 11 on Windows 7, Edge, Opera 17, Safari 9, Android 5.0, Java 8 |
+| `INTERMEDIATE` |	Firefox 1, Chrome 1, IE 7, Opera 5, Safari 1, Windows XP IE8, Android 2.3, Java 7 |
+| `OLD` |	Windows XP IE6, Java 6 | 
 
 ### When certificates are updated, the event handler will:
 - Move the resulting certificates to `/etc/nginx/ssl`
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+# Updated on April 17, 2016
+# TLS Protocols and cipher suites recommended by Mozilla
+# https://wiki.mozilla.org/Security/Server_Side_TLS
+# Be sure to escape !'s
+declare -A TLS_SETTING_PROTOS;
+TLS_SETTING_PROTOS["MODERN"]="TLSv1.2" 
+TLS_SETTING_PROTOS["INTERMEDIATE"]="TLSv1 TLSv1.1 TLSv1.2" 
+TLS_SETTING_PROTOS["OLD"]="SSLv3 TLSv1 TLSv1.1 TLSv1.2" 
+	
+declare -A TLS_SETTING_CIPHER;
+TLS_SETTING_CIPHER["MODERN"]="ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256" 
+TLS_SETTING_CIPHER["INTERMEDIATE"]="ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:\!DSS" 
+TLS_SETTING_CIPHER["OLD"]="ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:DES-CBC3-SHA:HIGH:SEED:\!aNULL:\!eNULL:\!EXPORT:\!DES:\!RC4:\!MD5:\!PSK:\!RSAPSK:\!aDH:\!aECDH:\!EDH-DSS-DES-CBC3-SHA:\!KRB5-DES-CBC3-SHA:\!SRP" 
+	
+
 
 # Check to see if let's encrypt has certificates already issued to a volume. 
 # If so, immediately overwrite fake certificates.
@@ -11,11 +26,13 @@ if [ -z "$LE_DOMAIN" ] && [ -z "$LE_EMAIL" ] && [ -z "$PROXY_DEST" ]; then
 else 
 	# Generate a local fake cert so nginx will start regardless of the success of LE, if it's configured to run.
 	echo "DOCKER NGINX LET'S ENCRYPT: Generate temporary certificate";
-	# Parse domains
+	# Parse domains and Proxy destinations
 	IFS=',' read -ra DOMAINS <<< "$LE_DOMAIN"
+	IFS=',' read -ra DESTINATIONS <<< "$PROXY_DEST"
 
 	openssl req -passout pass: -subj "/C=US/ST=CA/L=San Diego/O=$DOMAINS/OU=TS/CN=$DOMAINS/emailAddress=support@$DOMAINS" -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 
+	# Check previous existence of certificates 
 	echo "DOCKER NGINX LET'S ENCRYPT: Checking for previous certificate existence";
 	LE_CERT="/etc/letsencrypt/live/$LE_DOMAIN/fullchain.pem"
 	LE_KEY="/etc/letsencrypt/live/$LE_DOMAIN/privkey.pem"
@@ -27,11 +44,49 @@ else
 		echo "DOCKER NGINX LET'S ENCRYPT: No certificates found.";
 	fi
 
+	
+	# Determine TLS_SETTING
+	# Check to see if env is set and it's one of MODERN, INTERMEDIATE, or OLD.
+	# If check fails, set to MODERN
+	# [[ ${array[$test1]} ]];
+	if [ -z "$TLS_SETTING" ] && [[ ${TLS_SETTING_CIPHER["$TLS_SETTING"]} ]]; then
 
-	echo "DOCKER NGINX LET'S ENCRYPT: render nginx configuration with proxy details";
-	cat /etc/nginx/sites-available/webapp.1.conf > /etc/nginx/sites-enabled/webapp.conf
-	echo "        proxy_pass          $PROXY_DEST;" >> /etc/nginx/sites-enabled/webapp.conf
-	cat /etc/nginx/sites-available/webapp.2.conf >> /etc/nginx/sites-enabled/webapp.conf
+		echo "DOCKER NGINX LET'S ENCRYPT: TLS_SETTING set to $TLS_SETTING";	
+	else
+
+		echo "DOCKER NGINX LET'S ENCRYPT: TLS_SETTING not set. Using MODERN";
+		TLS_SETTING="MODERN";
+	fi
+
+	#GENERATE DHPARAM
+	echo "DOCKER NGINX LET'S ENCRYPT: Generating DH parameters";
+	openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+
+	echo "DOCKER NGINX LET'S ENCRYPT: render nginx configuration with proxy and destination details details";
+	echo "" > /etc/nginx/sites-enabled/webapp.conf
+	CT=0
+	for i in "${DOMAINS[@]}"; do
+		# By default, grab the first PROXY_DEST in the array
+	    THIS_DEST="$DESTINATIONS"
+	    if [ CT -lt ${#DESTINATIONS[@]} ]; then
+		    # Get right VALUE
+		    THIS_DEST="${DESTINATIONS[$CT]}"
+		fi
+	    cat /etc/nginx/sites-available/webapp.1.conf >> /etc/nginx/sites-enabled/webapp.conf
+
+	    echo "	ssl_protocols	${TLS_SETTING_PROTOS["$TLS_SETTING"]}";
+	    echo "	ssl_ciphers '${TLS_SETTING_CIPHER["$TLS_SETTING"]}'";
+
+	    echo "	server_name $i;" >> /etc/nginx/sites-enabled/webapp.conf
+	    echo "  location / {" >> /etc/nginx/sites-enabled/webapp.conf
+
+	    
+	    echo "        proxy_pass          $THIS_DEST;" >> /etc/nginx/sites-enabled/webapp.conf
+	    cat /etc/nginx/sites-available/webapp.2.conf >> /etc/nginx/sites-enabled/webapp.conf
+
+	    CT=$(($CT + 1))
+	done
+	
 fi
 
 
